89e174a5bbdadae82970d5cb02edcdd994fa411f
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-08-09 07:44:23 +0530

    setting up the checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -19,9 +19,49 @@ const Checkout = (props) => {
   };
   if (cart.length < 1) return <EmptyCart />;
   return (
-    <div>
-      <h1>hello from checkout page</h1>;
-    </div>
+    <section className="section form">
+      <h2 className="section-title">checkout</h2>
+      <form className="checkout-form">
+        <h3>
+          order total: <span>${total}</span>
+        </h3>
+
+        <div className="form-control">
+          <label htmlFor="name">name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="stripe-info">
+          <label htmlFor="card-element">Credit or Debit Card</label>
+          <p className="stripe-info">
+            Test using credit card: <span>4242 42424 4242 4242</span>
+            <br />
+            enter any 5 digits for zip code
+            <br />
+            enter any 3 digits for cvc
+          </p>
+        </div>
+        {error && <p className="form-empty">{error}</p>}
+        {isEmpty ? (
+          <p className="form-empty">please fill out name field</p>
+        ) : (
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="btn btn-primary btn-block"
+          >
+            submit{" "}
+          </button>
+        )}
+      </form>
+    </section>
   );
 };
 
