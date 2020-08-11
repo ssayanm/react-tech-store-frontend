a52b4a391ce4b9656adb62d7752e48ffcbd5b073
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-08-11 08:42:27 +0530

    updated action, cart and reducer

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,6 +1,13 @@
-import React, { createContext, useReducer, useEffect } from "react";
+import React, { createContext, useReducer, useEffect, useState } from "react";
 // import localCart from "../utils/localCart";
 import reducer from "./reducer.js";
+import {
+  REMOVE_ITEM_FROM_CART,
+  INCREASE_ITEM_FROM_CART,
+  DECREASE_ITEM_FROM_CART,
+  ADD_ITEM_TO_CART,
+  CLEAR_ITEM_FROM_CART,
+} from "./actions";
 
 const getCartFromLocalStorage = () => {
   return localStorage.getItem("cart")
@@ -12,18 +19,18 @@ export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cart, dispatch] = useReducer(reducer, getCartFromLocalStorage());
-  const [total, setTotal] = useReducer(0);
-  const [cartItems, setCartItems] = useReducer(0);
+  const [total, setTotal] = useState(0);
+  const [cartItems, setCartItems] = useState(0);
 
   useEffect(() => {
     // local storage
     localStorage.setItem("cart", JSON.stringify(cart));
-    //cart items
+    // cart items
     let newCartItems = cart.reduce((total, cartItem) => {
       return (total += cartItem.amount);
     }, 0);
     setCartItems(newCartItems);
-    //cart total
+    // cart total
     let newTotal = cart.reduce((total, cartItem) => {
       return (total += cartItem.amount * cartItem.price);
     }, 0);
@@ -33,34 +40,34 @@ const CartProvider = ({ children }) => {
 
   //remove item
   const removeItem = (id) => {
-    dispatch({ type: "REMOVE_ITEM_FROM_CART", payload: id });
+    dispatch({ type: REMOVE_ITEM_FROM_CART, payload: id });
   };
   //increase amount
   const increaseAmount = (id) => {
-    dispatch({ type: "INCREASE_ITEM_FROM_CART", payload: id });
+    dispatch({ type: INCREASE_ITEM_FROM_CART, payload: id });
   };
 
   //decrease amount
   const decreaseAmount = (id, amount) => {
     if (amount === 1) {
-      dispatch({ type: "REMOVE_ITEM_FROM_CART", payload: id });
+      dispatch({ type: REMOVE_ITEM_FROM_CART, payload: id });
       return;
     } else {
-      dispatch({ type: "DECREASE_ITEM_FROM_CART", payload: id });
+      dispatch({ type: DECREASE_ITEM_FROM_CART, payload: id });
     }
   };
   //add to cart
   const addToCart = (product) => {
     let item = [...cart].find((item) => item.id === product.id);
     if (item) {
-      dispatch({ type: "INCREASE_ITEM_FROM_CART", payload: product.id });
+      dispatch({ type: INCREASE_ITEM_FROM_CART, payload: product.id });
     } else {
-      dispatch({ type: "ADD_ITEM_TO_CART", payload: product });
+      dispatch({ type: ADD_ITEM_TO_CART, payload: product });
     }
   };
   //clear cart
   const clearCart = () => {
-    dispatch({ type: "CLEAR_ITEM_FROM_CART" });
+    dispatch({ type: CLEAR_ITEM_FROM_CART });
   };
 
   return (
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,34 +1,42 @@
+import {
+  REMOVE_ITEM_FROM_CART,
+  INCREASE_ITEM_FROM_CART,
+  DECREASE_ITEM_FROM_CART,
+  ADD_ITEM_TO_CART,
+  CLEAR_ITEM_FROM_CART,
+} from "./actions";
+
 export default (state, action) => {
   switch (action.type) {
-    case "TOGGLE_CART_HIDDEN":
-      return {
-        ...state,
-        hidden: !state.hidden,
-      };
+    // case TOGGLE_CART_HIDDEN:
+    //   return {
+    //     ...state,
+    //     hidden: !state.hidden,
+    //   };
 
-    case "ADD_ITEM_TO_CART":
+    case ADD_ITEM_TO_CART:
       const { id, image, title, price } = action.payload;
       let product = { id, image, title, price };
       return [...state, product];
 
-    case "REMOVE_ITEM_FROM_CART":
+    case REMOVE_ITEM_FROM_CART:
       return state.filter((item) => item.id !== action.payload);
 
-    case "INCREASE_ITEM_FROM_CART":
+    case INCREASE_ITEM_FROM_CART:
       return state.map((item) => {
         return item.id === action.payload
           ? { ...item, amount: item.amount + 1 }
           : { ...item };
       });
 
-    case "DECREASE_ITEM_FROM_CART":
+    case DECREASE_ITEM_FROM_CART:
       return state.map((item) => {
         return item.id === action.payload
           ? { ...item, amount: item.amount - 1 }
           : { ...item };
       });
 
-    case "CLEAR_ITEM_FROM_CART":
+    case CLEAR_ITEM_FROM_CART:
       return [];
 
     default:
