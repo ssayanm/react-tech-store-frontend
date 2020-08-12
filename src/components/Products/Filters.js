5892a5febeabdeb1694fa0c8ef251870bbf748a3
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-08-12 07:13:05 +0530

    updated the product context and product filter page

--- src/components/Products/Filters.js
+++ src/components/Products/Filters.js
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductContext } from "../../context/products";
 
-const FIlters = () => {
-  return <div>hello</div>;
+const Filters = () => {
+  const {
+    filters: { search, category, shipping, price },
+    updateFilters,
+    sorted,
+  } = useContext(ProductContext);
+  return (
+    <section className="filters-section">
+      <h2 className="section-title">search products</h2>
+      <form className="filters-form">
+        <div>
+          <div className="form-group">
+            <label htmlFor="search">search term</label>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              className="form-control"
+              value={search}
+              onChange={updateFilters}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="category">category</label>
+            <select
+              name="category"
+              id="category"
+              className="form-control"
+              value={category}
+              onChange={updateFilters}
+            >
+              <option value="all">all</option>
+              <option value="phone">phone</option>
+              <option value="radio">radio</option>
+              <option value="computer">computer</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="checkbox"
+              name="shipping"
+              id="shipping"
+              checked={shipping}
+              onChange={updateFilters}
+            />
+            <label htmlFor="shipping">free shipping</label>
+          </div>
+        </div>
+
+        <div className="price-group">
+          <p>price</p>
+          <label>
+            <input
+              type="radio"
+              name="price"
+              value="all"
+              checked={price === "all"}
+              onChange={updateFilters}
+            />
+            all
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="price"
+              value="0"
+              checked={price === "0"}
+              onChange={updateFilters}
+            />
+            ₹0 - ₹100
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="price"
+              value="100"
+              checked={price === 100}
+              onChange={updateFilters}
+            />
+            ₹100 - ₹200
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="price"
+              value="200"
+              checked={price === 200}
+              onChange={updateFilters}
+            />
+            Over ₹200
+          </label>
+        </div>
+      </form>
+      <h6>total products: {sorted.flat().length}</h6>
+      <hr />
+    </section>
+  );
 };
 
-export default FIlters;
+export default Filters;
--- src/context/products.js
+++ src/context/products.js
@@ -1,52 +1,101 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import axios from "axios";
 import url from "../utils/URL";
 import { featuredProducts, flattenProducts, paginate } from "../utils/helpers";
-
-export const ProductContext = createContext();
+export const ProductContext = React.createContext();
 
 const ProductProvider = ({ children }) => {
-  const [loading, setLoading] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [featured, setFeatured] = useState([]);
-  const [sorted, setSorted] = useState([]);
-  const [page, setPage] = useState(0);
-  const [filters, setFilters] = useState({
+  const [loading, setLoading] = React.useState(false);
+  const [products, setProducts] = React.useState([]);
+  const [featured, setFeatured] = React.useState([]);
+  // extra state values
+  const [sorted, setSorted] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [filters, setFilters] = React.useState({
     search: "",
     category: "all",
     shipping: false,
     price: "all",
   });
 
+  const changePage = (index) => {
+    setPage(index);
+  };
+
+  const updateFilters = (e) => {
+    const type = e.target.type;
+    const filter = e.target.name;
+    const value = e.target.value;
+    let filterValue;
+    if (type === "checkbox") {
+      filterValue = e.target.checked;
+    } else if (type === "radio") {
+      value === "all" ? (filterValue = value) : (filterValue = parseInt(value));
+    } else {
+      filterValue = value;
+    }
+    setFilters({ ...filters, [filter]: filterValue });
+  };
+
   useEffect(() => {
     setLoading(true);
     axios.get(`${url}/products`).then((response) => {
-      const featuredProduct = featuredProducts(flattenProducts(response.data));
+      const featured = featuredProducts(flattenProducts(response.data));
       const products = flattenProducts(response.data);
-
-      setSorted(paginate(products));
       setProducts(products);
-      setFeatured(featuredProduct);
+      setSorted(paginate(products));
+      setFeatured(featured);
       setLoading(false);
     });
     return () => {};
   }, []);
 
-  const changePage = (index) => {
-    setPage(index);
-  };
+  useLayoutEffect(() => {
+    let newProducts = [...products].sort((a, b) => a.price - b.price);
+    const { search, category, shipping, price } = filters;
+    //
+    if (category !== "all") {
+      newProducts = newProducts.filter((item) => item.category === category);
+    }
+    if (shipping !== false) {
+      newProducts = newProducts.filter(
+        (item) => item.free_shipping === shipping
+      );
+    }
+    if (price !== "all") {
+      newProducts = newProducts.filter((item) => {
+        if (price === 0) {
+          return item.price < 100;
+        } else if (price === 100) {
+          return item.price > 100 && item.price < 200;
+        } else {
+          return item.price >= 200;
+        }
+      });
+    }
 
-  const updateFilters = (e) => {};
+    if (search !== "") {
+      newProducts = newProducts.filter((item) => {
+        let title = item.title.toLowerCase().trim();
+        return title.startsWith(search) ? item : null;
+      });
+    }
+
+    setPage(0);
+
+    setSorted(paginate(newProducts));
+  }, [filters, products]);
+
   return (
     <ProductContext.Provider
       value={{
-        loading,
         products,
+        loading,
         featured,
         sorted,
         page,
-        filters,
         changePage,
+        filters,
         updateFilters,
       }}
     >
