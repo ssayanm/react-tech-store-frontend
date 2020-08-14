f7a426e22eb173159a51f274c5f938145632fda1
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-08-14 09:50:32 +0530

    updated the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,32 @@
 import React from "react";
+import aboutimage from "../assets/about.jpg";
 
 const About = () => {
   return (
-    <section className="section about-section">
+    <section className="section about-page">
+      {" "}
       <h1 className="section-title">About Us</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
-        architecto expedita recusandae. Harum obcaecati provident libero nostrum
-        temporibus, architecto ut debitis enim dolorem fugit facere beatae! Et
-        repellat veniam officiis?
-      </p>
+      <div className="about-section">
+        <div>
+          <img src={aboutimage} alt="about" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+          architecto expedita recusandae. Harum obcaecati provident libero
+          nostrum temporibus, architecto ut debitis enim dolorem fugit facere
+          beatae! Et repellat veniam officiis? Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Numquam architecto expedita recusandae.
+          Harum obcaecati provident libero nostrum temporibus, architecto ut
+          debitis enim dolorem fugit facere beatae! Et repellat veniam officiis?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+          architecto expedita recusandae. Harum obcaecati provident libero
+          nostrum temporibus, architecto ut debitis enim dolorem fugit facere
+          beatae! Et repellat veniam officiis? Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Numquam architecto expedita recusandae.
+          Harum obcaecati provident libero nostrum temporibus, architecto ut
+          debitis enim dolorem fugit facere beatae! Et repellat veniam officiis?
+        </p>
+      </div>
     </section>
   );
 };
